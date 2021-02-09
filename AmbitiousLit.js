b1f1007274ad49880b41ae1541fded607eb57ea8
Elias K. Salem <48525998+EKenjiSalem@users.noreply.github.com>
2021-02-09 11:24:24 -0800

    Add files via upload

---
+++ AmbitiousLit.js
@@ -0,0 +1,46 @@
+
+    
+  let search = document.getElementById('search');
+
+   const books = [
+     {
+       name: 'google',
+       Author: 'google',
+       url: 'https://www.google.com'
+       },
+       {
+         name: 'google',
+         Author: 'google',
+         url: 'https://www.google.com'
+ 
+     }
+   ];
+
+
+   search.addEventListener('keyup', searchList);
+
+
+
+   /*
+  function openPage() {
+  var books = document.getElementById("search").value;
+
+  if ( books === "Rakim") {
+      window.open("newarrivals/rakimhiphop.html");
+}
+
+  if ( books.toUpperCase() === "SWEAT THE TECHNIQUE") {
+    window.open("file:///C:/Users/Salem%20Family/Desktop/Ambitious%20Lit/newarrivals/rakimhiphop.html");
+} 
+
+  if ( books.toUpperCase() === "SNOOP DOGG" ) {
+      window.open("file:///C:/Users/Salem%20Family/Desktop/Ambitious%20Lit/culinary/crookhiphop.html");
+    }
+
+  
+    if ( books.toUpperCase() === "FROM CROOK TO COOK") {
+      window.open("file:///C:/Users/Salem%20Family/Desktop/Ambitious%20Lit/culinary/crookhiphop.html");
+  } 
+  }; 
+
+*/
